c484c0cc5960cc27cb645d8e68c394b86bdf7277
曾士珍 <sjtseng0924@gpu2.cplab.cs.nycu.edu.tw>
2025-12-19 15:47:27 +0800

    add: tdbear

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
-export LORA_PATH_CONTENT="./lora-sdxl-catnew/pytorch_lora_weights.safetensors"
+export LORA_PATH_CONTENT="./lora-sdxl-bear/pytorch_lora_weights.safetensors"
 export LORA_PATH_STYLE="lora-sdxl-waterpaintingnew/pytorch_lora_weights.safetensors"
-export OUTPUT_FOLDER="./anncba(fixabs)"  
+export OUTPUT_FOLDER="outputs-sdxl-bear-waterpaintingnew"  
 export PROMPT="ann's cat in cba style"
 
 python inference_sd.py \
